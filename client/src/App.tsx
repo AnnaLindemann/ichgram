6687b22f8f2241fbbfd27fc5f51116664674f45a
Anna <anshalneva@gmail.com>
2026-02-19 20:48:19 +0100

    setup client and server, docker-compose mongo, and db-aware healthcheck

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="p-6 text-2xl font-bold">
+       <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>  
+      </div>
+     
+    </>
+  )
+}
+
+export default App
