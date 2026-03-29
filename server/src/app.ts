39ea28fb75f762396cb1b3078a4d84e12fb872bd
Anna <anshalneva@gmail.com>
2026-03-29 16:42:59 +0200

    update cors back

--- server/src/app.ts
+++ server/src/app.ts
@@ -14,13 +14,18 @@ import { notificationsRouter } from "./modules/notifications/notifications.route
 
 export function createApp(){
   const app = express();
-  app.use(helmet());
+ 
  app.use(
   cors({
-    origin: process.env.CLIENT_URL ?? "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ichgram-wheat.vercel.app",
+      "https://ichgram-844snh9qx-annalindemanns-projects.vercel.app",
+    ],
     credentials: true,
   }),
-);
+); 
+app.use(helmet());
   app.use(morgan("dev"));
   app.use(express.json({limit: "2mb"}));
   app.use(
